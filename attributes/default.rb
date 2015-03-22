ff9794c984d4811d90db37720a0a22abf5fdf211
Vicent Gozalbes <vigosan@gmail.com>
2015-03-22 18:09:49 +0100

    Setup alert mail destination

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
 default[:monit][:check_interval] = 30
+default[:monit][:mailserver] = 'localhost'
+default[:monit][:mail][:from] = 'root@localhost'
+default[:monit][:mail][:to] = 'root@localhost'
 default[:monit][:mongo][:pidfile] = '/var/lib/mongo/mongod.lock'
